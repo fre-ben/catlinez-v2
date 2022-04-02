8e9b6e658ca0f4db97a45d543ba7876596f49513
Freben <fschocke@gmail.com>
2022-04-03 01:49:44 +0200

    Call express backend in Catline component

--- src/lib/Catline.svelte
+++ src/lib/Catline.svelte
@@ -1,5 +1,13 @@
+<script lang="ts">
+  async function fetchGIF() {
+    const gif = await fetch(`/api/cat`, { method: "GET" }).then((res) => res.json());
+
+    console.log(await gif);
+  }
+</script>
+
 <div>
-  <button class="btn btn-accent">German Catline</button>
+  <button class="btn btn-accent" on:click={async () => fetchGIF()}>German Catline</button>
   <button class="btn btn-accent">English Catline</button>
 </div>
 
