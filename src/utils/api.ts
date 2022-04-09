3ae79bf91d0440fed5760e826db8619f2f080ba6
Freben <fschocke@gmail.com>
2022-04-09 20:53:06 +0200

    Add getHeadline() function

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,4 @@
-import type { CatGif } from "../types";
+import type { CatGif, Headline } from "../types";
 
 /**
  * Gets Cat GIF from backend
@@ -12,5 +12,20 @@ export async function getCatGIF(): Promise<CatGif> {
     return catGif;
   } else {
     throw new Error(catGif);
+  }
+}
+
+/**
+ * Gets Headline from backend
+ * @returns Promise<Headline>
+ */
+export async function getHeadline(language: string): Promise<Headline> {
+  const response = await fetch(`/api/news/${language}`, { method: "GET" });
+  const headline = await response.json();
+
+  if (response.ok) {
+    return headline;
+  } else {
+    throw new Error(headline);
   }
 }
