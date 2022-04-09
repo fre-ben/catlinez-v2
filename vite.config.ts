09b8a9af9cb71dd81f34746972a9df6092798e8c
Freben <fschocke@gmail.com>
2022-04-09 17:11:31 +0200

    Add german and english news API route

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
     port: 1337,
     proxy: {
       "/api/cat": { target: "http://localhost:3000/", changeOrigin: true },
-      "/api/news": { target: "http://localhost:3000/", changeOrigin: true },
+      "/api/news/de": { target: "http://localhost:3000/", changeOrigin: true },
+      "/api/news/en": { target: "http://localhost:3000/", changeOrigin: true },
     },
   },
   build: {
