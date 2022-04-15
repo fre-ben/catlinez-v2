5ed6394cfa558f962cd7baafa4bfe25c7827246f
Freben <fschocke@gmail.com>
2022-04-15 19:45:10 +0200

    Add simple starter test for Catline component

---
+++ src/lib/Catline.test.ts
@@ -0,0 +1,12 @@
+import { cleanup, fireEvent, render, screen } from "@testing-library/svelte";
+import { describe, it, expect, afterEach } from "vitest";
+import Catline from "./Catline.svelte";
+
+describe("Catline component", () => {
+  afterEach(() => cleanup());
+
+  it("should render", () => {
+    const { container } = render(Catline);
+    expect(container).toBeTruthy();
+  });
+});
