6a37a846196b7a95cf53177499dbe7ab9ebebea1
Freben <fschocke@gmail.com>
2022-04-15 19:44:59 +0200

    Add test properties to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  // @ts-ignore
+  plugins: [svelte({ hot: !process.env.VITEST })],
   server: {
     port: 1337,
     proxy: {
@@ -14,5 +15,9 @@ export default defineConfig({
   },
   build: {
     outDir: "dist/app",
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
   },
 });
