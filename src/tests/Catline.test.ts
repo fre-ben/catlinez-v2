6b172ec0bbc9319c8ff6b9e5b4d2699466be308c
Freben <fschocke@gmail.com>
2022-04-15 19:54:41 +0200

    Move tests

--- src/tests/Catline.test.ts
+++ src/tests/Catline.test.ts
@@ -1,6 +1,6 @@
 import { cleanup, fireEvent, render, screen } from "@testing-library/svelte";
 import { describe, it, expect, afterEach } from "vitest";
-import Catline from "./Catline.svelte";
+import Catline from "../lib/Catline.svelte";
 
 describe("Catline component", () => {
   afterEach(() => cleanup());
